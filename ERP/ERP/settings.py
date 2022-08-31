06ea932366748a77696a03ecd71dcd1a6c388c90
Andriyevski <andriyevski@gmail.com>
2022-08-31 23:44:36 +0300

    Add DB for work with SQL

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'databaseName',
+        'USER': 'databaseUser',
+        'PASSWORD': 'databasePassword',
+        'HOST': 'localhost',
+        'PORT': 'portNumber',
     }
 }
 
--- ERP/form/models.py
+++ ERP/form/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    id_dep_client = models.TextField(max_length=100)
+    name_client = models.TextField(max_length=100)
+
+class Project_Num(models.Model):
+    id = models.IntegerField(unique=True)
+    id_dep_client = models.IntegerField()
+    id_project = models.PositiveIntegerField()
