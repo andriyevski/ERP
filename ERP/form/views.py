0b09dbcd8b4c8f3f2ee22bf7016ff84eab6461f3
Andriyevski <andriyevski@gmail.com>
2022-09-02 18:49:11 +0300

    Make a index files and rotes for css

--- ERP/form/views.py
+++ ERP/form/views.py
@@ -1,5 +1,15 @@
+from urllib import response
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.shortcuts import render
+from .models import Client
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi man!")
+    name = "ERP 'ba3a'"
+    client_num = 'Клиент №:'
+    project_name = 'Проект №:'
+
+    response = render_to_string('form/index.html')
+    return render(request,'form/index.html',{'client_num':client_num,
+    'project_name':project_name, 'name':name})
