4b40908a69e8ded27e690cfe50a02283a36da1f7
Andriyevski <andriyevski@gmail.com>
2022-09-02 22:42:35 +0300

    all static is fine

--- ERP/form/views.py
+++ ERP/form/views.py
@@ -2,7 +2,7 @@ from urllib import response
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.shortcuts import render
-from .models import Client
+from .models import Client,Project_Num
 
 # Create your views here.
 def index(request):
@@ -10,6 +10,5 @@ def index(request):
     client_num = 'Клиент №:'
     project_name = 'Проект №:'
 
-    response = render_to_string('form/index.html')
     return render(request,'form/index.html',{'client_num':client_num,
     'project_name':project_name, 'name':name})
